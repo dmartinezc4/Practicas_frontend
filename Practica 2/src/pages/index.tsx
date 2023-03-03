96936762d4d17f1eada5e2a6e7590a77b208b1db
David <43136464+dmartinezc4@users.noreply.github.com>
2023-03-03 08:37:59 +0100

    Update index.tsx

--- Practica 2/src/pages/index.tsx
+++ Practica 2/src/pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Formulario from "@/components/formulario"
 import Grid from "@/components/grid"
 import clearConst from "@/components/formulario"
 import Link from 'next/link'
@@ -22,4 +21,3 @@ export default function Home() {
   )
 }
 
-//El botón de Remove Top Row hace que se quite la primera fila del grid
