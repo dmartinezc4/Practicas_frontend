5a99d7cd02c24d8365d5abc10d8146a5c4b6909a
David <43136464+dmartinezc4@users.noreply.github.com>
2023-03-03 08:12:08 +0100

    Add files via upload

--- Practica 2/src/components/grid.tsx
+++ Practica 2/src/components/grid.tsx
@@ -1,32 +1,60 @@
 import { useState } from "react";
 import styled from "styled-components"
+import Image from 'next/image'
 
-const Grid = ()=>{
-    const [nombre,setNombre]=useState<string>("");//variable para el nombre
-    const [dni,setDni]=useState<string>("");//variable para el dni
+const Grid = () => {
+    const [nombre, setNombre] = useState<string>("");
+    const [dni, setDNI] = useState<string>("");
+    const [numFilas, setFilas] = useState<number>(0);
+
+    const isDNI = (dni: string): boolean => {
+        const formato = /^[0-9]{8,8}[A-Za-z]$/;
+      
+        if(formato.test(dni)==false){
+          console.log("DNI invalido, ejemplo de DNI valido:12345678A")
+        }else{
+          return formato.test(dni);
+        }
+        
+    }
     
-    //Aqui está lo que sería la tabla base (Nombre, Dni y 2 ejemplos)
+    const addRow = () => {
+        const tabla = document.getElementById("grid");
+        if(tabla != null){
+            setFilas(numFilas+1);
+            tabla.innerHTML += `
+            <div id=\"grid_val\">${nombre}</div><div id=\"grid_val\">${dni}</div>
+            <button>
+                <img src="/papelera.png" id=\"grid_val\"  width="17" height="20"/>
+            </button>
+        `;
+        }
+      }
+
     return(
         <>
-        
-         <div className="grid-container">
-        <div className="grid-header">Nombre</div>
-        <div className="grid-header">DNI</div>
-        <div className="grid-item">David</div>  
-        <div className="grid-item">12345678A</div>
-        <div className="grid-item">Jose</div>
-        <div className="grid-item">98765432Z</div>  
- 
-      </div>
 
-        
+            Introduce un nombre: <input id="nombre" type="text" value={nombre} onChange = {(e) => setNombre(e.target.value)}/><br/>
+            Introduce un DNI: <input id="dni" type="text" value={dni} onChange = {(e) => setDNI(e.target.value)}/><br/>
+            <button onClick={(e)=>{if((nombre && dni) && isDNI(dni)) addRow()}} ><img src="/añadir.png" id="grid_val"  width="20" height="20"/></button><br/>
 
+            <DivTabla filas={numFilas} id = "grid">
+            </DivTabla><br/>
+            
         </>
-    )
+    );
 }
 
+type TablaProps = {
+    filas: number
+}
 
-
-
+const DivTabla = styled.div<TablaProps>`
+    display: inline-grid;
+    border: 1px solid black;
+    background-color: #cdcdcd;
+    grid-template-columns: 200px 200px 30px;
+    grid-template-rows: repeat(${props => props.filas}, 30px);
+`
 
 export default Grid;
--- Practica 2/src/pages/index.tsx
+++ Practica 2/src/pages/index.tsx
@@ -5,61 +5,19 @@ import styles from '@/styles/Home.module.css'
 import Formulario from "@/components/formulario"
 import Grid from "@/components/grid"
 import clearConst from "@/components/formulario"
+import Link from 'next/link'
+
 const inter = Inter({ subsets: ['latin'] })
 
-export function clearInputs(){
-  document.getElementById("input_n").value = "";
-  document.getElementById("input_d").value = "";
-}
 
-export function appendGrid(n,i){
-  var name= n;
-  var id=i;
-  document.getElementById("grid_id").innerHTML +=
-  "<div class=\"grid-item\" id=\"grid_val\">"+n+
-  "</div><div class=\"grid-item\" id=\"grid_val\">" +i +
-  "</div>";
-  clearInputs();
-}
 
-function removeFirst(){
-  if(document.getElementById("grid_val")==null){
-    //Si no hay items en el grid no hacemos la funcion y le mandamos al user un aviso por consola de que no hay más por eliminar
-    console.log("There is only the handlers")
-    return;
-  }
-  console.log("removing");
-  
-  let grid = document.getElementById("grid_id");
-  let node = document.getElementById("grid_val");
-  console.log(node);
-  
-  let throwawayNode = grid.removeChild(node);
-  grid = document.getElementById("grid_id");
-  node = document.getElementById("grid_val");
-  throwawayNode = grid.removeChild(node);
-  console.log(node);
-  
-}
+
 
 
 export default function Home() {
-
-  
   return (
     <>
-      <Formulario id="form_id"/> 
-      
-      <div className="grid-container" id="grid_id">
-        <div className="grid-header">Nombre</div>
-        <div className="grid-header">DNI</div>
-        <div className="grid-item" id="grid_val">David</div>  
-        <div className="grid-item" id="grid_val">12345678A</div>
-        <div className="grid-item" id="grid_val">Jose</div>
-        <div className="grid-item" id="grid_val">98765432Z</div> 
-      </div>
-      
-      <button onClick={(e)=>removeFirst()} >Remove Top Row</button> 
+    <Link href="/table"> Ir a la tabla</Link><br/>
     </>
   )
 }
