d50ff00941ab2d8fffd91d6ce88719f4f35daf1a
David <43136464+dmartinezc4@users.noreply.github.com>
2023-03-03 08:39:39 +0100

    Add files via upload

--- Practica 2/src/pages/table.tsx
+++ Practica 2/src/pages/table.tsx
@@ -2,9 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Formulario from "@/components/formulario"
 import Grid from "@/components/grid"
-import clearConst from "@/components/formulario"
 import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -60,4 +58,3 @@ export default function Home() {
   )
 }
 
-//El botón de Remove Top Row hace que se quite la primera fila del grid
